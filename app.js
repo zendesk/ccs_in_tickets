396702d02bcdcd3262fcfcc82f03b41f2efaba1a
Pierre Nespo <pierre.nespo@gmail.com>
2013-01-07 12:52:07 +0100

    Display ticket's collaborators if any

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 (function() {
     return {
+
+        events: {
+            'app.activated':               'onActivated',
+            'ticket.status.changed':       'loadIfDataReady'
+        },
+
+        onActivated: function() {
+            this.doneLoading = false;
+            this.loadIfDataReady();
+        },
+
+        loadIfDataReady: function() {
+            if ( !this.doneLoading && this.ticket() !== null) {
+                this.doneLoading = true;
+                this.displayCollaborators();
+            }
+        },
+
+        displayCollaborators: function(){
+            var collaborators = this.ticket().collaborators();
+
+            if (_.size(collaborators) > 0) {
+                this.switchTo('collaborators', {
+                    collaborators: _.map(collaborators, function(cc){
+                        return cc.name();
+                    })
+                });
+            }
+        }
+
     };
 }());
