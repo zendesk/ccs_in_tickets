9daca836d9274c88ec36c96d791b11ea935d369b
pierre <pierre.nespo@gmail.com>
2014-01-08 16:01:06 +0000

    Update app to match the recent changes in the framework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-ccs_in_ticket_app
-=================
+:warning: *Use of this software is subject to important terms and conditions as set forth in the License file* :warning:
+
+# CCs in ticket
+
+By downloading this app, you are agreeing to our [terms and conditions](https://github.com/zendesklabs/wiki/wiki/Terms-and-Conditions)
--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 (function() {
-    return {
+  return {
+   events: {
+      'app.activated'                     : 'onAppActivated',
+      'ticket.collaborators.changed'      : 'displayCollaborators',
+      'click .toggle'                     : 'toggleApp'
+    },
 
-        events: {
-            'app.activated'                     : 'onActivated',
-            'ticket.status.changed'             : 'loadIfDataReady',
-            'ticket.collaborators.changed'      : 'displayCollaborators',
-            'click .toggle'                     : 'toggleApp'
-        },
+    onAppActivated: function(app) {
+      if (app.firstLoad) {
+        this.initialize();
+      }
+    },
 
-        onActivated: function() {
-            this.doneLoading = false;
-            this.loadIfDataReady();
-        },
+    initialize: function() {
+      this.ensureTrayVisibility();
+      this.displayCollaborators();
+    },
 
-        loadIfDataReady: function() {
-            if (!this.doneLoading && this.ticket() !== null) {
-                this.doneLoading = true;
-                this.ensureTrayVisibility();
-                this.displayCollaborators();
-            }
-        },
+    ensureTrayVisibility: function(){
+      var appTray = services.appsTray();
 
-        ensureTrayVisibility: function(){
-            var tray = services.appsTray();
+      if (!appTray.isVisible()) {
+        appTray.show();
+      }
+    },
 
-            if (!tray.isVisible())
-                tray.show();
-        },
+    toggleApp: function(){
+      if (this.$('section[data-main]').is(':visible')){
+        this.$('section[data-main]').hide();
+        this.$('.toggle').text(this.I18n.t('toggle-show'));
+      } else {
+        this.$('section[data-main]').show();
+        this.$('.toggle').text(this.I18n.t('toggle-hide'));
+      }
+    },
 
-        toggleApp: function(){
-            if (this.$('section[data-main]').is(':visible')){
-                this.$('section[data-main]').hide();
-                this.$('.toggle').text(this.I18n.t('toggle-show'));
-            } else {
-                this.$('section[data-main]').show();
-                this.$('.toggle').text(this.I18n.t('toggle-hide'));
-            }
-        },
+    displayCollaborators: function(){
+      var collaborators = this.ticket().collaborators(),
+      collaborator_size = _.size(collaborators);
 
-        displayCollaborators: function(){
-            var collaborators = this.ticket().collaborators(),
-            collaborator_size = _.size(collaborators);
+      if (collaborator_size > 0) {
+        this.$('h3 small').html('<strong>('+ collaborator_size +')</strong>');
 
-            if (collaborator_size > 0) {
-                this.$('h3 small').html('<strong>('+ collaborator_size +')</strong>');
+        this.switchTo('collaborators', {
+          collaborators: _.map(collaborators, function(cc){
+            return cc.email();
+          })
+        });
 
-                this.switchTo('collaborators', {
-                    collaborators: _.map(collaborators, function(cc){
-                        return cc.email();
-                    })
-                });
+        _.times(8, function(){
+          this.$('.alert').fadeToggle('slow');
+        }, this);
+      } else {
+        this.$('h3 small').html('');
+        this.$('section[data-main]').html('');
+      }
+    }
 
-                _.times(8, function(){
-                    this.$('.alert').fadeToggle('slow');
-                }, this);
-            }
-        }
-
-    };
+  };
 }());
